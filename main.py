18c59d11d302c3f7394981cefe2bbb737b6595d0
kmiprieto <kamiprieto30@gmail.com>
2021-10-04 22:53:37 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template, request, flash
+
+import utils
+from forms import ContactUs
+
+import yagmail as yagmail
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    try:
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+
+            if not utils.isEmailValid(email):
+                error = "El email no es valido"
+                flash(error)
+                return render_template('register.html')
+
+            if not utils.isUsernameValid(username):
+                error = "El usuario no es valido"
+                flash(error)
+                return render_template('register.html')
+
+            if not utils.isPasswordValid(password):
+                error = "El password no es valido"
+                flash(error)
+                return render_template('register.html')
+
+            return render_template('login.html')
+        return render_template('register.html')
+    except Exception as e:
+        return render_template('register.html')
+
+
+@app.route('/contactUs', methods=['GET', 'POST'])
+def contactUs():
+    form = ContactUs()
+    return render_template('contactUs.html', form=form)
